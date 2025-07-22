439eb367453cf71b0fcd4f0a066633045bb50c16
Giordano <giordanob056@gmail.com>
2025-07-22 14:19:13 -0300

    ORM de busca por ID criada

--- ORM.js
+++ ORM.js
@@ -1,10 +1,11 @@
 
 const { syncDatabase } = require('./sync');
 const express = require('express')
-const {updateUser} = require('./update');
+const { updateUser } = require('./update');
 const { createUser } = require('./create');
 const { listUsers } = require('./read');
-const {deleteUser} = require('./delete');
+const { deleteUser } = require('./delete');
+const { getUserById } = require('./getUserById');
 
 const app = express()
 
@@ -12,15 +13,15 @@ app.use(express.json())
 
 app.post('/user', async (request, response) => {
     createUser(
-        request.body.name, 
-        request.body.email, 
+        request.body.name,
+        request.body.email,
         request.body.phone)
-        .then(()=>{
+        .then(() => {
             response.status(200).json({
-                message:'Contact created'
+                message: 'Contact created'
             })
         })
-        .catch((error)=>{
+        .catch((error) => {
             response.status(500).json({
                 erro: error
             })
@@ -28,27 +29,27 @@ app.post('/user', async (request, response) => {
 })
 
 
-app.patch('/update/:id', async(request,response)=>{
+app.patch('/update/:id', async (request, response) => {
     const id = request.params.id;
     const updatedData = request.body
-    
-    try{
-        const result = await updateUser(id,updatedData);
+
+    try {
+        const result = await updateUser(id, updatedData);
         response.status(200).json(result)
     }
-    catch(error){
-        response.status(500).json({error:error.message})
+    catch (error) {
+        response.status(500).json({ error: error.message })
     }
 })
 
 
-app.delete('/delete/:id',async(request,response)=>{
+app.delete('/delete/:id', async (request, response) => {
     const id = request.params.id;
-    try{
+    try {
         const result = await deleteUser(id)
         response.status(200).json(result)
     }
-    catch(error){
+    catch (error) {
         response.status(500).json({
             error: error.message
         })
@@ -56,12 +57,32 @@ app.delete('/delete/:id',async(request,response)=>{
 })
 
 
-app.get('/user', async (request,response)=>{
-    try{
+app.get('/user/:id', async (request, response) => {
+    const { id } = request.params;
+    try {
+        const user = await getUserById(id)
+        if (!user) {
+            return response.status(404).json({
+                message: 'Contact not found'
+            })
+        }
+        response.status(200).json(user);
+    }
+    catch (error) {
+        response.status(500).json({
+            error: error.message
+        })
+    }
+})
+
+
+
+app.get('/user', async (request, response) => {
+    try {
         const users = await listUsers()
         response.status(200).json(users);
     }
-    catch(error){
+    catch (error) {
         response.status(500).json(error)
     }
 })
